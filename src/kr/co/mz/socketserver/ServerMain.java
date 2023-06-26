d8df71cd0281da73bc5105c87266a0a9cb6c5784
mz01-junghunee <cjswovkdnj12@naver.com>
2023-06-26 13:29:26 +0900

    MY SocketServer

--- src/kr/co/mz/socketserver/ServerMain.java
+++ src/kr/co/mz/socketserver/ServerMain.java
@@ -5,7 +5,7 @@ import kr.co.mz.socketserver.server.Server;
 
 public class ServerMain {
     private static final int PORT = 8080;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         // 서버 소켓 생성
         try(Server server = new Server(PORT)) {
             server.start();
